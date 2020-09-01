1e5cf310da2793b3379b10e8dc70487642a9d52b
kun.xie <kun.xie@mobvoi.com>
2020-09-01 16:23:18 +0800

    order服务和user服务注册到eureka中

--- app-order/src/main/java/com/mdl/alkb/server/order/AppOrderApplication.java
+++ app-order/src/main/java/com/mdl/alkb/server/order/AppOrderApplication.java
@@ -2,12 +2,14 @@ package com.mdl.alkb.server.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient//代表自己是一个服务提供方
 public class AppOrderApplication {
 
   public static void main(String[] args) {
-    System.setProperty("spring.config.location", "classpath:/configs_order.properties");
+    System.setProperty("spring.config.location", "classpath:/application_order.yml");
     SpringApplication.run(AppOrderApplication.class, args);
   }
 
--- app-user-center/src/main/java/com/mdl/alkb/server/user/AppUserCenterApplication.java
+++ app-user-center/src/main/java/com/mdl/alkb/server/user/AppUserCenterApplication.java
@@ -2,12 +2,14 @@ package com.mdl.alkb.server.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient//代表自己是一个服务提供方
 public class AppUserCenterApplication {
 
   public static void main(String[] args) {
-    System.setProperty("spring.config.location","classpath:/configs.properties");
+    System.setProperty("spring.config.location","classpath:/application_user.yml");
     SpringApplication.run(AppUserCenterApplication.class, args);
   }
 
